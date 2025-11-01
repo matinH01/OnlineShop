081522a35fae273080cdf4b952c1dbfbd43553ea
matinH01 <matinhamdolahi117@gmail.com>
2025-11-01 16:27:39 +0330

    Update AppDestination.kt

--- app/src/main/java/net/holosen/onlineshop/ui/screen/AppDestination.kt
+++ app/src/main/java/net/holosen/onlineshop/ui/screen/AppDestination.kt
@@ -1,5 +1,33 @@
 package net.holosen.onlineshop.ui.screen
 
-sealed interface AppDestination {
+import kotlinx.serialization.Serializable
 
+sealed interface AppDestination {
+    @Serializable
+    data object Home : AppDestination
+    @Serializable
+    data object Basket : AppDestination
+    @Serializable
+    data object UserPayment : AppDestination
+    @Serializable
+    data object Login : AppDestination
+    @Serializable
+    data object Profile : AppDestination
+    @Serializable
+    data object Invoices : AppDestination
+    @Serializable
+    data class Products(
+        val catId: Long,
+        val title: String
+    ) : AppDestination
+
+    @Serializable
+    data class Product(
+        val id: Long
+    ) : AppDestination
+
+    @Serializable
+    data class Invoice(
+        val id: Long
+    ) : AppDestination
 }
